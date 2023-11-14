317503408e1be97b26fa9f01c7e468a4ac593d42
Karoline Cruz <karollcruz@hotmail.com>
2023-11-14 18:14:41 +0000

    Learning basic concepts of useState and Props.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,14 @@
-
 import ListGroup from "./components/ListGroup";
+let cities = ["Paris", "London", "Berlin", "Hong Kong", "Tokyo"];
+let colors = ["Red", "Green", "Blue", "Orange", "Black"];
 
 function App() {
-  return <div><ListGroup /></div>
+  return (
+    <div>
+      <ListGroup items={cities} heading="Cities" />
+      <ListGroup items={colors} heading="Colors" />
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,20 +1,26 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  let items = ["Paris", "London", "Berlin", "Hong Kong", "Tokyo"];
-  //items = [];
-  const handleClick = (event: MouseEvent) => console.log(event);
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={handleClick} 
+            onClick={() => setSelectedIndex(index)}
           >
             {item}
           </li>
